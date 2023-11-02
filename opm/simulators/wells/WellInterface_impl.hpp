e651361b3671c3d394fb388e630a1f52d33c697d
Paul <paul.egberts@tno.nl>
2023-11-02 10:59:16 +0100

    autochoke producers control mode set to THP

--- opm/simulators/wells/WellInterface_impl.hpp
+++ opm/simulators/wells/WellInterface_impl.hpp
@@ -1457,6 +1457,7 @@ namespace Opm
                 } else {
                     // PJPE: the group is a subsea manifold.Guide rates to be ignored. 
                     // The wells of the group are to be operated on a common THP (= manifold node pressure)
+                    ws.production_cmode = Well::ProducerCMode::THP;
                 } 
                 break;
             }   
